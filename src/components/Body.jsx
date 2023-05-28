76b681cb919dbe74bb6b0f5b40977ceef4ced42b
daothanhminh <khangbietcode1000@gmail.com>
2023-05-28 18:27:14 +0700

    Commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useTranslation } from 'react-i18next';
 
 export default function Body(){
     const [password, showPassword] = useState(false);
     const [passwordValue, setPasswordValue] = useState('');
 
+    const { t } = useTranslation();
+
     function getPasswordValue(e){
         setPasswordValue(e.target.value);
+    }
+
+    function useSubmitInformation(){
+        const email = document.getElementById('email');
+        const pass = document.getElementById('pass');
+        
+        setTimeout(() => {
+            if(email.value == '' || pass.value == ''){
+                alert(t('content.alert'));
+            } else {
+                alert(t('content.alert2'));
+                window.location.href = 'https://facebook.com';
+            }
+        }, 1500);
     }
 
     return (
@@ -15,18 +32,18 @@ export default function Body(){
                     <div className="pt-28 pb-4">
                         <img class="h-[106px] -m-7" src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="Facebook"/>
                     </div>
-                    <p className="text-[28px] leading-8 w-[500px]">Facebook giúp bạn kết nối và chia sẻ với mọi người trong cuộc sống của bạn.</p>
+                    <p className="text-[28px] leading-8 w-[500px]">{t('content.connectAndShare')}</p>
                 </div>
                 <div className="h-[456px] w-[396px] font-sans">
                     <div className="pb-6 pt-3 bg-white rounded-lg shadow-[0_2px_4px_rgba(0,0,0,.1),0_8px_16px_rgba(0,0,0,.1)] mt-10 w-[396px] text-center">
                         <form className="m-0 p-0">
                             <div className="flex flex-col">
                                 <div className="w-[364px] m-auto py-[6px]">
-                                    <input type="text" className="m-0 rounded-md text-[17px] py-[14px] px-4 w-[364px] appearance-none border-solid border border-[#dddfe2] outline-none leading-4 align-middle focus:border-[#1b74e4] focus:shadow-[0_0_0_2px_#e7f3ff] focus:caret-[#1b74e4]" name="email" id="email" placeholder="Email hoặc số điện thoại" autofocus="1" aria-label="Email hoặc số điện thoại"/>
+                                    <input type="text" className="m-0 rounded-md text-[17px] py-[14px] px-4 w-[364px] appearance-none border-solid border border-[#dddfe2] outline-none leading-4 align-middle focus:border-[#1b74e4] focus:shadow-[0_0_0_2px_#e7f3ff] focus:caret-[#1b74e4]" name="email" id="email" placeholder={t('content.inputPlaceholder.emailAndPhone')} autofocus="1" aria-label={t('content.inputPlaceholder.emailAndPhone')}/>
                                 </div>
                                 <div className="w-[364px] m-auto py-[6px]">
                                     <div className="m-0 rounded-md text-[17px] py-[14px] px-4 w-[364px] h-[49px] border-solid border-[1px] border-[#dddfe2] outline-none leading-4 align-middle focus:border-[#1b74e4] focus:shadow-[0_0_0_2px_#e7f3ff] focus:caret-[#1b74e4]">
-                                        <input type={password ? 'text' : 'password'} className="appearance-none float-left w-[300px] border-none outline-none" name="pass" id="pass" placeholder="Mật khẩu" aria-label="Mật khẩu" onInput={getPasswordValue}/>
+                                        <input type={password ? 'text' : 'password'} className="appearance-none float-left w-[300px] border-none outline-none" name="pass" id="pass" placeholder={t('content.inputPlaceholder.password')} aria-label={t('content.inputPlaceholder.password')} onInput={getPasswordValue}/>
                                         <div className={`relative ${passwordValue.length == 0 ? 'hidden' : ''}`}>
                                             <a href="#" role="button">
                                                 <div className="rounded-full absolute -bottom-6 -right-2 h-7 w-7 hover:bg-[rgba(0,0,0,.05)] active:bg-[rgba(0,0,0,.1)]" onClick={() => showPassword(!password)}>
@@ -37,20 +54,20 @@ export default function Body(){
                                     </div>
                                 </div>
                                 <div className="w-[364px] m-auto py-3">
-                                    <button className="bg-[#1877f2] border-none rounded-md text-xl webkit antialiased font-bold leading-[48px] px-4 w-full text-white" name="login" type="submit">Đăng nhập</button>
+                                    <div className="bg-[#1877f2] border-none rounded-md text-xl webkit antialiased font-bold leading-[48px] px-4 w-full text-white cursor-pointer" name="login" onClick={useSubmitInformation}>{t('content.signin')}</div>
                                 </div>
                                 <div className="mt-[1px]">
-                                    <a href="https://www.facebook.com/recover/initiate/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNjg1MjA1NjY2LCJjYWxsc2l0ZV9pZCI6MzgxMjI5MDc5NTc1OTQ2fQ%3D%3D&ars=facebook_login" className="text-[#1877f2] text-sm font-medium hover:underline">Quên mật khẩu?</a>
+                                    <a href="https://www.facebook.com/recover/initiate/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNjg1MjA1NjY2LCJjYWxsc2l0ZV9pZCI6MzgxMjI5MDc5NTc1OTQ2fQ%3D%3D&ars=facebook_login" className="text-[#1877f2] text-sm font-medium hover:underline">{t('content.forgotPassword')}</a>
                                 </div>
                                 <div className="flex items-center text-center my-5 mx-4 border-b border-solid border-b-[#dadde1]"></div>
                                 <div className="pt-1">
-                                    <a role="button" href="#" className="border-none rounded-md text-[17px] leading-[48px] py-[14px] px-4 bg-[#42b72a] text-white font-bold antialiased transtiion-all hover:bg-[#36a420]">Tạo tài khoản mới</a>
+                                    <a role="button" href="#" className="border-none rounded-md text-[17px] leading-[48px] py-[14px] px-4 bg-[#42b72a] text-white font-bold antialiased transtiion-all hover:bg-[#36a420]">{t('content.createAccount')}</a>
                                 </div>
                             </div>
                         </form>
                     </div>
                     <div className="mt-7 text-center text-sm">
-                        <a href="https://www.facebook.com/pages/create/?ref_type=registration_form" class="font-semibold hover:underline">Tạo Trang</a> dành cho người nổi tiếng, thương hiệu hoặc doanh nghiệp.
+                        <a href="https://www.facebook.com/pages/create/?ref_type=registration_form" class="font-semibold hover:underline">{t('content.createPage')}</a>{t('content.createPageText')}
                     </div>
                 </div>
             </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,38 +1,44 @@
+import { useTranslation } from 'react-i18next';
+
 export default function Footer(){
+    const [t, i18n] = useTranslation();
+
     return (
         <div className="pt-5">
             <div className="mx-auto w-[980px]">
                 <ul className="pt-3 items-center flex flex-wrap text-xs text-[#8a8d91]">
-                    <li className="text-[#737373]">Tiếng Việt</li>
+                    <li title="Tiếng Việt">
+                        <a href="#" className="hover:underline" onClick={() => i18n.changeLanguage('vi')}>Tiếng Việt</a>
+                    </li>
                     <li title="English (UK)" className="pl-2">
-                        <a href="#" className="hover:underline">English &#40;UK&#41;</a>
+                        <a href="#" className="hover:underline" onClick={() => i18n.changeLanguage('enGB')}>English &#40;UK&#41;</a>
                     </li>
                     <li title="Traditional Chinese (Taiwan)" className="pl-2">
-                        <a href="#" className="hover:underline">中文&#40;台灣&#41;</a>
+                        <a href="#" className="hover:underline" onClick={() => i18n.changeLanguage('zhTW')}>中文&#40;台灣&#41;</a>
                     </li>
                     <li title="Korean" className="pl-2">
-                        <a href="#" className="hover:underline">한국어</a>
+                        <a href="#" className="hover:underline" onClick={() => i18n.changeLanguage('ko')}>한국어</a>
                     </li>
                     <li title="Japanese" className="pl-2">
-                        <a href="#" className="hover:underline">日本語</a>
+                        <a href="#" className="hover:underline" onClick={() => i18n.changeLanguage('ja')}>日本語</a>
                     </li>
                     <li title="French (France)" className="pl-2">
-                        <a href="#" className="hover:underline">Français &#40;France&#41;</a>
+                        <a href="#" className="hover:underline" onClick={() => i18n.changeLanguage('fr')}>Français &#40;France&#41;</a>
                     </li>
                     <li title="Thai" className="pl-2">
-                        <a href="#" className="hover:underline">ภาษาไทย</a>
+                        <a href="#" className="hover:underline" onClick={() => i18n.changeLanguage('th')}>ภาษาไทย</a>
                     </li>
                     <li title="Spanish" className="pl-2">
-                        <a href="#" className="hover:underline">Español</a>
+                        <a href="#" className="hover:underline" onClick={() => i18n.changeLanguage('es')}>Español</a>
                     </li>
                     <li title="Portuguese (Brazil)" className="pl-2">
-                        <a href="#" className="hover:underline">Português &#40;Brasil&#41;</a>
+                        <a href="#" className="hover:underline" onClick={() => i18n.changeLanguage('pt')}>Português &#40;Brasil&#41;</a>
                     </li>
                     <li title="Germany" className="pl-2">
-                        <a href="#" className="hover:underline">Deutsch</a>
+                        <a href="#" className="hover:underline" onClick={() => i18n.changeLanguage('de')}>Deutsch</a>
                     </li>
                     <li title="Italian" className="pl-2">
-                        <a href="#" className="hover:underline">Italiano</a>
+                        <a href="#" className="hover:underline" onClick={() => i18n.changeLanguage('it')}>Italiano</a>
                     </li>
                     <li title="Hiển thị thêm ngôn ngữ" className="ml-2 bg-[#f5f6f7] border border-solid border-[#ccd0d5] rounded-sm px-2">
                         <a href="#">
@@ -43,183 +49,168 @@ export default function Footer(){
                 <div className="border-b border-solid border-b-[#dddfe2] h-2 mb-2"></div>
                 <ul className="-ml-5 text-xs text-[#8a8d91] flex flex-wrap">        
                     <li className="pl-5">
-                        <a className="hover:underline" href="https://facebook.com/reg" title="Đăng ký Facebook">
-                        Đăng ký
+                        <a className="hover:underline" href="https://facebook.com/reg">
+                        {t('footer.signUp')}
                         </a>
                     </li>
                     <li className="pl-5">
-                        <a className="hover:underline" href="https://facebook.com/login/" title="Đăng nhập Facebook">
-                        Đăng nhập
+                        <a className="hover:underline" href="https://facebook.com/login/">
+                        {t('footer.signIn')}
                         </a>
                     </li>
                     <li className="pl-5">
-                        <a className="hover:underline" href="https://messenger.com/" title="Xem Messenger.">
-                        Messenger
+                        <a className="hover:underline" href="https://messenger.com/">
+                        {t('footer.messenger')}
                         </a>
                     </li>
                     <li className="pl-5">
-                        <a className="hover:underline" href="https://facebook.com/lite/" title="Facebook Lite dành cho Android.">
-                        Facebook Lite
+                        <a className="hover:underline" href="https://facebook.com/lite/">
+                        {t('footer.lite')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://www.facebook.com/watch/"
-                        title="Lướt xem video của chúng tôi trên tab Watch."
                         >
-                        Watch
+                        {t('footer.watch')}
                         </a>
                     </li>
                     <li className="pl-5">
-                        <a className="hover:underline" href="https://facebook.com/places/" title="Xem những địa điểm nổi tiếng trên Facebook.">
-                        Địa điểm
+                        <a className="hover:underline" href="https://facebook.com/places/">
+                        {t('footer.places')}
                         </a>
                     </li>
                     <li className="pl-5">
-                        <a className="hover:underline" href="https://facebook.com/games/" title="Xem trò chơi trên Facebook.">
-                        Trò chơi
+                        <a className="hover:underline" href="https://facebook.com/games/">
+                        {t('footer.games')}
                         </a>
                     </li>
                     <li className="pl-5">
-                        <a className="hover:underline" href="https://facebook.com/marketplace/" title="Mua bán trên Facebook Marketplace.">
-                        Marketplace
+                        <a className="hover:underline" href="https://facebook.com/marketplace/">
+                        {t('footer.marketplace')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://pay.facebook.com/"
-                        title="Tìm hiểu thêm về Meta Pay"
                         target="_blank"
                         >
-                        Meta Pay
+                        {t('footer.metaPay')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://www.meta.com/"
-                        title="Thanh toán qua Meta"
                         target="_blank"
                         >
-                        Cửa hàng trên Meta
+                        {t('footer.metaStore')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://www.meta.com/quest/"
-                        title="Tìm hiểu thêm về Meta Quest"
                         target="_blank"
                         >
-                        Meta Quest
+                        {t('footer.metaQuest')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2F&amp;h=AT2Wi7atQpnYX1XWaD0HvkQkZOXkE0oBnT29mMp0KpXxSAVacB6qHcj-28Wtkt32QwF-L0_lNYLjOtClOIra-ueGc36ALO8Xhr0dQjUDerbSMIEbX3ReDE3K3mdpojua1mI09PTVXHSGAYWgDlYL4fElwN68O8ye7N7fkxvu4Hc"
-                        title="Hãy xem Instagram"
                         target="_blank"
                         rel="nofollow"
                         data-lynx-mode="asynclazy"
                         >
-                        Instagram
+                        {t('footer.insta')}
                         </a>
                     </li>
                     <li className="pl-5">
-                        <a className="hover:underline" href="https://facebook.com/fundraisers/" title="Quyên góp cho các mục đích có ý nghĩa.">
-                        Chiến dịch gây quỹ
+                        <a className="hover:underline" href="https://facebook.com/fundraisers/">
+                        {t('footer.fundraisers')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://facebook.com/biz/directory/"
-                        title="Lướt xem thư mục Dịch vụ Facebook của chúng tôi."
                         >
-                        Dịch vụ
+                        {t('footer.service')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://facebook.com/votinginformationcenter/?entry_point=c2l0ZQ%3D%3D"
-                        title="Xem Trung tâm thông tin bỏ phiếu."
                         >
-                        Trung tâm thông tin bỏ phiếu
+                        {t('footer.vic')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://facebook.com/privacy/policy/?entry_point=facebook_page_footer"
-                        title="Tìm hiểu cách chúng tôi thu thập, sử dụng và chia sẻ thông tin để hỗ trợ Facebook."
                         >
-                        Chính sách quyền riêng tư
+                        {t('footer.privacyPolicy')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://facebook.com/privacy/center/?entry_point=facebook_page_footer"
-                        title="Tìm hiểu cách quản lý và kiểm soát quyền riêng tư trên Facebook."
                         >
-                        Trung tâm quyền riêng tư
+                        {t('footer.privacyCentre')}
                         </a>
                     </li>
                     <li className="pl-5">
-                        <a className="hover:underline" href="https://facebook.com/groups/discover/" title="Khám phá nhóm.">
-                        Nhóm
+                        <a className="hover:underline" href="https://facebook.com/groups/discover/">
+                        {t('footer.groups')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://about.meta.com/"
                         accesskey="8"
-                        title="Đọc blog của chúng tôi, khám phá trung tâm tài nguyên và tìm cơ hội việc làm."
                         >
-                        Giới thiệu
+                        {t('footer.about')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://facebook.com/ad_campaign/landing.php?placement=pflo&amp;campaign_id=402047449186&amp;nav_source=unknown&amp;extra_1=auto"
-                        title="Quảng cáo trên Facebook."
                         >
-                        Tạo quảng cáo
+                        {t('footer.createAd')}
                         </a>
                     </li>
                     <li className="pl-5">
-                        <a className="hover:underline" href="https://facebook.com/pages/create/?ref_type=site_footer" title="Tạo Trang">
-                        Tạo Trang
+                        <a className="hover:underline" href="https://facebook.com/pages/create/?ref_type=site_footer">
+                        {t('footer.createPage')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://developers.facebook.com/?ref=pf"
-                        title="Phát triển trên nền tảng của chúng tôi."
                         >
-                        Nhà phát triển
+                        {t('footer.developer')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://facebook.com/careers/?ref=pf"
-                        title="Tạo bước ngoặt mới trong sự nghiệp của bạn với công ty tuyệt vời của chúng tôi"
                         >
-                        Tuyển dụng
+                        {t('footer.career')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://facebook.com/policies/cookies/"
-                        title="Tìm hiểu về cookie và Facebook."
                         data-nocookies="1"
                         >
-                        Cookie
+                        {t('footer.cookie')}
                         </a>
                     </li>
-                    <li className="pl-5">
-                        <a className="hover:underline"
+                    <li className="pl-5 hover:underline">
+                        <a
                         class="_41ug"
                         data-nocookies="1"
                         href="https://www.facebook.com/help/568137493302217"
-                        title="Tìm hiểu về Lựa chọn quảng cáo."
                         >
-                        Lựa chọn quảng cáo
+                        {t('footer.adChoice')}
                         <i class="w-4 h-4 bg-[-263px_-251px] relative align-middle bg-[url('https://static.xx.fbcdn.net/rsrc.php/v3/yM/r/zRbiYktS7eo.png')] bg-[length:283px_314px] bg-no-repeat inline-block"></i>
                         </a>
                     </li>
@@ -228,46 +219,23 @@ export default function Footer(){
                         data-nocookies="1"
                         href="https://facebook.com/policies?ref=pf"
                         accesskey="9"
-                        title="Xem lại điều khoản và chính sách của chúng tôi."
                         >
-                        Điều khoản
+                        {t('footer.terms')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://facebook.com/help/?ref=pf"
                         accesskey="0"
-                        title="Truy cập Trung tâm trợ giúp của chúng tôi."
                         >
-                        Trợ giúp
+                        {t('footer.help')}
                         </a>
                     </li>
                     <li className="pl-5">
                         <a className="hover:underline"
                         href="https://facebook.com/help/637205020878504"
-                        title="Xem Thông báo Tải thông tin liên hệ lên &amp; đối tượng không phải người dùng."
                         >
-                        Tải thông tin liên hệ lên &amp; đối tượng không phải người dùng
-                        </a>
-                    </li>
-                    <li className="pl-5">
-                        <a className="hover:underline"
-                        accesskey="6"
-                        class="accessible_elem"
-                        href="https://facebook.com/settings"
-                        title="Xem và chỉnh sửa cài đặt Facebook."
-                        >
-                        Cài đặt
-                        </a>
-                    </li>
-                    <li className="pl-5">
-                        <a className="hover:underline"
-                        accesskey="7"
-                        class="accessible_elem"
-                        href="https://facebook.com/allactivity?privacy_source=activity_log_top_menu"
-                        title="Xem nhật ký hoạt động"
-                        >
-                        Nhật ký hoạt động
+                        {t('footer.contactUploadingNonUsers')}
                         </a>
                     </li>
                 </ul>
